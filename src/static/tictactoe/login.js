de62924820f71d7bb3c58d3877908f076f05bb31
Shuo Tang <tangshuo@gmail.com>
2013-09-17 15:56:09 -0700

    login/sign up button

--- src/static/tictactoe/login.js
+++ src/static/tictactoe/login.js
@@ -2,7 +2,31 @@ $(document).ready(function() {
     $("#login-form").submit(function(event) {
         var username = $("#username-input").val();
         var passwd = $("#password-input").val();
+        event.stopPropagation();
 
+        localStorage.clear();
+        adrenaline.user.logIn(username, passwd).done(function() {
+            console.log("logged in: " + err);
+            adrenaline.user.saveCurrentUserToDisk();
+            window.location.href = "tictactoe.html";
+        }).fail(function(err) {
+            console.log("could not sign up: " + err);
+        });
+        return false;
+    });
+
+    $("#signup-button").click(function(event) {
+        if (!$("#username-input")[0].checkValidity() ||
+            !$("#password-input")[0].checkValidity()) {
+            $("#login-button").click();
+            return false;
+        }
+
+
+        var username = $("#username-input").val();
+        var passwd = $("#password-input").val();
+
+        localStorage.clear();
         adrenaline.user.signUp(username, passwd).done(function() {
             adrenaline.user.saveCurrentUserToDisk();
             window.location.href = "tictactoe.html";
@@ -11,4 +35,5 @@ $(document).ready(function() {
         });
         return false;
     });
+
 });
